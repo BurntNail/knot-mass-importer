c8bdc0e73553077c1a1b65290c61a891a9a61807
Jack Maguire/BurntNail <github.29x6f@simplelogin.com>
2023-06-15 16:08:46 +0000

    fix bugs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use sqlx::postgres::PgPoolOptions;
 use csv_async::AsyncReaderBuilder;
 use futures::stream::StreamExt;
 use chrono::naive::{NaiveDateTime, NaiveDate, NaiveTime};
+use std::collections::HashMap;
 
 pub struct PersonPlusEvents {
 	pub first_name: String,
@@ -30,20 +31,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut events = vec![];
     let mut combined = vec![];
 
+    let participants: HashMap<String, i32> = sqlx::query!("SELECT CONCAT(first_name, ' ', surname) AS full_name, id FROM people").fetch_all(&pool).await?.into_iter().flat_map(|x| x.full_name.map(|f| (f, x.id))).collect();
+
 
 	let mut i = 0;
     while let Some(record) = rdr.next().await.transpose()? {
     	if i == 0 {
     		events = record.iter().skip(2).map(|x| x.to_string()).collect::<Vec<_>>();
     	} else if i == 1{
-    		let mut years = record.iter().skip(2).map(|x| x.parse());
+    		let mut years = record.iter().skip(2).map(|x| x.parse::<i32>()).collect::<Result<Vec<_>, _>>()?;
+    		println!("{years:?}");
     		for name in &events {
+    			let date = NaiveDateTime::new(NaiveDate::from_ymd_opt(years.remove(0), 9, 1).unwrap(), NaiveTime::from_hms_opt(9, 0, 0).unwrap());
+
+				println!("{date:?}");
+    		
     			let id = sqlx::query!(
     			            r#"
     			INSERT INTO events (event_name, date, location, teacher) 
     			VALUES ($1, $2, $3, $4) RETURNING id"#,
     			            name,
-    			            NaiveDateTime::new(NaiveDate::from_ymd_opt(years.next().expect("expected_year")?, 9, 1).unwrap(), NaiveTime::from_hms_opt(09, 00, 00).unwrap()),
+    			            date,
     			            "KCS",
     			            "MJPC"
     			     
@@ -54,21 +62,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     			combined.push(id.id);
     		}
     	} else {
-    		let first_name = record[0].to_string();
-    		let surname = record[1].to_string();
-    		let form = record[2].to_string();
-    		let events = record.iter().skip(3).flat_map(|x| x.parse::<usize>().ok());
+    		let full_name = record[0].to_string();
+    		let form = record[1].to_string();
+    		let events = record.iter().skip(2).map(|x| !x.is_empty()).enumerate().flat_map(|(i, part)|if part {Some(combined[i])} else {None} );
 
-    		let Ok(participant_id) = sqlx::query!("SELECT id FROM people WHERE first_name = $1 and surname = $2 and form = $3", first_name, surname, form).fetch_one(&pool).await else {
+    		let Some(participant_id) = participants.get(&full_name) else {
 				let events = events.collect::<Vec<_>>();
-    			eprintln!("Unable to do {first_name} {surname} in {form} with {events:?}");
+    			eprintln!("Unable to do {full_name} in {form} with {events:?}");
     			continue;
     		};
-    		let participant_id = participant_id.id;
     		println!("Doing {participant_id}");
 
     		let mut count = 0;
-    		for event_db_id in events.map(|i| combined[i]) {
+    		for event_db_id in events {
     			sqlx::query!(
     			                r#"
     			    INSERT INTO public.participant_events
@@ -84,7 +90,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     			count += 1;
     		}
 
-    		println!("Added {first_name} {surname} to {count} events.");
+    		println!("Added {full_name} to {count} events.");
     	}
     	
     	i += 1;
